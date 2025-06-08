59aada5d7a79402978187a6451b1af4ec49061f6
saJeremyQin <343220868@qq.com>
2025-06-08 11:23:00 +0930

    revise directory, add bootstrap

---
+++ infra/main/backend.tf
@@ -0,0 +1,11 @@
+
+// enable use_lockfile to avoid state file being applied at the same tim
+terraform {
+  backend "s3" {
+    bucket       = "emenu-terraform-state-bucket"
+    key          = "env/dev/terraform.tfstate"
+    region       = "ap-southeast-2"
+    encrypt      = true
+    use_lockfile = true  
+  }
+}
