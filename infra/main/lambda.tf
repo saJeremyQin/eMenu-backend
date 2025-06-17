46b3abcb9c945ecfdba6a43da09dd6fe118f3fec
saJeremyQin <343220868@qq.com>
2025-06-17 17:42:41 +0930

    update timeout, add terraform init

--- infra/main/lambda.tf
+++ infra/main/lambda.tf
@@ -12,7 +12,7 @@ resource "aws_lambda_function" "emenu_server" {
   handler       = "index.handler"
   runtime       = "nodejs20.x"
   role          = aws_iam_role.lambda_exec.arn
-  timeout       = 13
+  timeout       = 14
 
   environment {
     variables = {
