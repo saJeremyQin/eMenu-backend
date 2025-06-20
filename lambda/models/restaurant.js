538e2b401e023a6f0b49a13f5d5b495ecac8e646
saJeremyQin <343220868@qq.com>
2025-06-20 21:36:29 +0930

    remove tojson in restaurnat.js

--- lambda/models/restaurant.js
+++ lambda/models/restaurant.js
@@ -27,14 +27,6 @@ const restaurantSchema = new mongoose.Schema({
 }, 
 {
     timestamps: true,
-    toJSON: {
-        virtuals: true, // 启用虚拟属性
-        transform: (doc, ret) => {
-            ret.id = ret._id.toString(); // 将 _id 映射到 id
-            delete ret._id;              // 移除原始的 _id 属性
-            delete ret.__v;              // 移除 __v 属性 (Mongoose 版本控制)
-        }
-    }
 });
 
 const Restaurant = mongoose.models.Restaurant || mongoose.model('Restaurant', restaurantSchema);
