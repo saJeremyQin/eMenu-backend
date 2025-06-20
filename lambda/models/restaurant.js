37a148f433a5b782caab795a4f0f4577fdaedfc9
saJeremyQin <343220868@qq.com>
2025-06-20 21:20:41 +0930

    Convert _id to id manually

--- lambda/models/restaurant.js
+++ lambda/models/restaurant.js
@@ -26,7 +26,15 @@ const restaurantSchema = new mongoose.Schema({
     },
 }, 
 {
-    timestamps: true
+    timestamps: true,
+    toJSON: {
+        virtuals: true, // 启用虚拟属性
+        transform: (doc, ret) => {
+            ret.id = ret._id.toString(); // 将 _id 映射到 id
+            delete ret._id;              // 移除原始的 _id 属性
+            delete ret.__v;              // 移除 __v 属性 (Mongoose 版本控制)
+        }
+    }
 });
 
 const Restaurant = mongoose.models.Restaurant || mongoose.model('Restaurant', restaurantSchema);
