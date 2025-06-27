1644ac8ebde8e23637d35495cb647ef1463e2975
saJeremyQin <343220868@qq.com>
2025-06-27 17:15:35 +0930

    update layer deploy workflow with base64, update comments of restaurant.js

--- lambdas/layers/common_models/nodejs/models/restaurant.js
+++ lambdas/layers/common_models/nodejs/models/restaurant.js
@@ -28,8 +28,8 @@ const restaurantSchema = new mongoose.Schema({
 {
     timestamps: true,
     toJSON: {
-      virtuals: true,        // 添加虚拟字段（id）
-      versionKey: false,     // 去掉 __v 字段
+      virtuals: true,                 // add virtual field（id）
+      versionKey: false,              // 去掉 __v 字段
       transform: (_, ret) => {
         ret.id = ret._id.toString();  // 映射 _id -> id
         delete ret._id;               // 删除 _id，避免重复
