9b63e24726eb2d356b08da98dfeb49dce1601152
saJeremyQin <343220868@qq.com>
2025-06-27 17:22:49 +0930

    update arn of layer name with common_mongoose_models, update restaurant.js

--- lambdas/layers/common_models/nodejs/models/restaurant.js
+++ lambdas/layers/common_models/nodejs/models/restaurant.js
@@ -29,7 +29,7 @@ const restaurantSchema = new mongoose.Schema({
     timestamps: true,
     toJSON: {
       virtuals: true,                 // add virtual field（id）
-      versionKey: false,              // 去掉 __v 字段
+      versionKey: false,              // remove __v field
       transform: (_, ret) => {
         ret.id = ret._id.toString();  // 映射 _id -> id
         delete ret._id;               // 删除 _id，避免重复
