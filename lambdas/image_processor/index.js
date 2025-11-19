738e909b13b63067ebd71fb7f99915ff0ea323a5
saJeremyQin <343220868@qq.com>
2025-11-19 19:14:21 +1030

    trigger image_processor deployment

--- lambdas/image_processor/index.js
+++ lambdas/image_processor/index.js
@@ -4,6 +4,7 @@ import { buildS3AndProcessedKeys } from './image_uploader/index.js';
 
 const s3Client = new S3Client({ region: process.env.AWS_REGION || "ap-southeast-2" });
 
+
 export const handler = async (event) => {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
